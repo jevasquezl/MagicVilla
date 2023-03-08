cdeae2a48f8aa0bbc9ec4373e8091fa3eddfc0da
Julio Vasquez <je_vasquez@hotmail.com>
2023-03-08 11:04:59 -0600

    Primero

---
+++ MagicVilla_API/Migrations/20230308151817_inicial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaAPI.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 3, 8, 9, 18, 17, 619, DateTimeKind.Local).AddTicks(7092), new DateTime(2023, 3, 8, 9, 18, 17, 619, DateTimeKind.Local).AddTicks(7082) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 3, 8, 9, 18, 17, 619, DateTimeKind.Local).AddTicks(7094), new DateTime(2023, 3, 8, 9, 18, 17, 619, DateTimeKind.Local).AddTicks(7094) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 1, 14, 16, 14, 27, 223, DateTimeKind.Local).AddTicks(6466), new DateTime(2023, 1, 14, 16, 14, 27, 223, DateTimeKind.Local).AddTicks(6413) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 1, 14, 16, 14, 27, 223, DateTimeKind.Local).AddTicks(6475), new DateTime(2023, 1, 14, 16, 14, 27, 223, DateTimeKind.Local).AddTicks(6472) });
+        }
+    }
+}
